58c667b07a98959704e4f1bdae9f9e830df5d9ed
maija <maijaaaw@gmail.com>
2023-01-14 17:26:51 +0200

    add sidebar, header, footer

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,23 @@
 import {
     MantineProvider,
-    Text,
     AppShell,
     Center,
-    Group,
-    Stack,
+    Navbar,
+    useMantineTheme,
+    Text,
+    MediaQuery,
+    Aside,
+    Footer,
+    Header,
+    Burger,
 } from '@mantine/core';
-import { Tile } from './tiles/Tile';
+import { useState } from 'react';
+import { TileList } from './TileList';
 
 const App = () => {
+    const theme = useMantineTheme();
+    const [opened, setOpened] = useState(false);
+
     return (
         <MantineProvider
             withGlobalStyles
@@ -17,61 +26,61 @@ const App = () => {
                 colorScheme: 'dark',
             }}
         >
-            <AppShell>
+            <AppShell
+                navbarOffsetBreakpoint="sm"
+                asideOffsetBreakpoint="sm"
+                navbar={
+                    <Navbar
+                        p="md"
+                        hiddenBreakpoint="sm"
+                        hidden={!opened}
+                        width={{ sm: 200, lg: 300 }}
+                    >
+                        <Text>Application navbar</Text>
+                    </Navbar>
+                }
+                footer={
+                    <Footer
+                        height={{ base: 40, md: 50 }}
+                        p={{ base: 'xs', md: 'md' }}
+                    >
+                        Application footer
+                    </Footer>
+                }
+                header={
+                    <Header
+                        height={{ base: 30, md: 40 }}
+                        p={{ base: 'md', md: 'md' }}
+                    >
+                        <div
+                            style={{
+                                display: 'flex',
+                                alignItems: 'center',
+                                height: '100%',
+                            }}
+                        >
+                            <MediaQuery
+                                largerThan="sm"
+                                styles={{ display: 'none' }}
+                            >
+                                <Burger
+                                    opened={opened}
+                                    onClick={() => setOpened(o => !o)}
+                                    size="sm"
+                                    color={theme.colors.gray[6]}
+                                    mr="xl"
+                                />
+                            </MediaQuery>
+
+                            <Text weight={600} size={'lg'}>
+                                Learn Riichi Tiles
+                            </Text>
+                        </div>
+                    </Header>
+                }
+            >
                 <Center>
-                    <Stack>
-                        <Group>
-                            <Tile code={'M1'} showName />
-                            <Tile code={'M2'} showName />
-                            <Tile code={'M3'} showName />
-                            <Tile code={'M4'} showName />
-                            <Tile code={'M5'} showName />
-                            <Tile code={'M5d'} showName />
-                            <Tile code={'M6'} showName />
-                            <Tile code={'M7'} showName />
-                            <Tile code={'M8'} showName />
-                            <Tile code={'M9'} showName />
-                        </Group>
-
-                        <Group>
-                            <Tile code={'P1'} showName />
-                            <Tile code={'P2'} showName />
-                            <Tile code={'P3'} showName />
-                            <Tile code={'P4'} showName />
-                            <Tile code={'P5'} showName />
-                            <Tile code={'P5d'} showName />
-                            <Tile code={'P6'} showName />
-                            <Tile code={'P7'} showName />
-                            <Tile code={'P8'} showName />
-                            <Tile code={'P9'} showName />
-                        </Group>
-
-                        <Group>
-                            <Tile code={'S1'} showName />
-                            <Tile code={'S2'} showName />
-                            <Tile code={'S3'} showName />
-                            <Tile code={'S4'} showName />
-                            <Tile code={'S5'} showName />
-                            <Tile code={'S5d'} showName />
-                            <Tile code={'S6'} showName />
-                            <Tile code={'S7'} showName />
-                            <Tile code={'S8'} showName />
-                            <Tile code={'S9'} showName />
-                        </Group>
-
-                        <Group>
-                            <Tile code={'WE'} showName />
-                            <Tile code={'WS'} showName />
-                            <Tile code={'WN'} showName />
-                            <Tile code={'WW'} showName />
-                        </Group>
-
-                        <Group>
-                            <Tile code={'DR'} showName />
-                            <Tile code={'DW'} showName />
-                            <Tile code={'DG'} showName />
-                        </Group>
-                    </Stack>
+                    <TileList />
                 </Center>
             </AppShell>
         </MantineProvider>
---
+++ src/components/TileList.tsx
@@ -0,0 +1,60 @@
+import { Group, Stack } from '@mantine/core';
+import { Tile } from './tiles/Tile';
+
+export const TileList = () => {
+    return (
+        <Stack>
+            <Group>
+                <Tile code={'M1'} showName />
+                <Tile code={'M2'} showName />
+                <Tile code={'M3'} showName />
+                <Tile code={'M4'} showName />
+                <Tile code={'M5'} showName />
+                <Tile code={'M5d'} showName />
+                <Tile code={'M6'} showName />
+                <Tile code={'M7'} showName />
+                <Tile code={'M8'} showName />
+                <Tile code={'M9'} showName />
+            </Group>
+
+            <Group>
+                <Tile code={'P1'} showName />
+                <Tile code={'P2'} showName />
+                <Tile code={'P3'} showName />
+                <Tile code={'P4'} showName />
+                <Tile code={'P5'} showName />
+                <Tile code={'P5d'} showName />
+                <Tile code={'P6'} showName />
+                <Tile code={'P7'} showName />
+                <Tile code={'P8'} showName />
+                <Tile code={'P9'} showName />
+            </Group>
+
+            <Group>
+                <Tile code={'S1'} showName />
+                <Tile code={'S2'} showName />
+                <Tile code={'S3'} showName />
+                <Tile code={'S4'} showName />
+                <Tile code={'S5'} showName />
+                <Tile code={'S5d'} showName />
+                <Tile code={'S6'} showName />
+                <Tile code={'S7'} showName />
+                <Tile code={'S8'} showName />
+                <Tile code={'S9'} showName />
+            </Group>
+
+            <Group>
+                <Tile code={'WE'} showName />
+                <Tile code={'WS'} showName />
+                <Tile code={'WN'} showName />
+                <Tile code={'WW'} showName />
+            </Group>
+
+            <Group>
+                <Tile code={'DR'} showName />
+                <Tile code={'DW'} showName />
+                <Tile code={'DG'} showName />
+            </Group>
+        </Stack>
+    );
+};
