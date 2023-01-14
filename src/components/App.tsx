57c154c0475188467a8a764b9585a6cc89648087
maija <maijaaaw@gmail.com>
2023-01-14 15:22:43 +0200

    add mantine and tile svgs

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,26 @@
-function App() {
-    return <div>Learn Riichi Tiles</div>;
-}
+import { MantineProvider, Text, AppShell, Center } from '@mantine/core';
+import { Tile } from './tiles/Tile';
+
+const App = () => {
+    return (
+        <>
+            <Tile />
+            <MantineProvider
+                withGlobalStyles
+                withNormalizeCSS
+                theme={{
+                    colorScheme: 'dark',
+                }}
+            >
+                <AppShell>
+                    <Center>
+                        <Text>Learn Riichi Tiles</Text>
+                        <Tile />
+                    </Center>
+                </AppShell>
+            </MantineProvider>
+        </>
+    );
+};
 
 export default App;
---
+++ src/components/tiles/Tile.tsx
@@ -0,0 +1,10 @@
+import { ThemeIcon } from '@mantine/core';
+import { ReactComponent as Man1 } from './../../svgs/white_tiles/Man1.svg';
+
+export const Tile = () => {
+    return (
+        <ThemeIcon color={'white'}>
+            <Man1 />
+        </ThemeIcon>
+    );
+};
